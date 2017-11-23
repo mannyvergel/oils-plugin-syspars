d0fe7f7930e7a320d587dc2619a587c920582a65
Manny Vergel <mvergel@azeus.com.rp>
2017-11-23 16:46:32 +0800

    Transferred default value of createDt to pre-save to avoid returning value even if it's nil

--- models/SysPar.js
+++ models/SysPar.js
@@ -4,7 +4,19 @@ module.exports = {
   schema: {
     key: {type: String, index: true, unique: true},
     val: String,
-    createDt: {type: Date, default: Date.now}
+    createDt: {type: Date}
+  },
+
+  initSchema: function(mySchema) {
+  	mySchema.pre('save', function(next) {
+  		//the problem with using "default" is that even when it's empty
+  		//mongoose is returning the value as Date.now
+  		if (!this.createDt) {
+  			this.createDt = new Date();
+  		}
+
+  		next();
+  	});
   }
 
 }
