b24f7fbaf5031d310a7ea03d3082cb5cae1a21f1
Manny <manny@mvergel.com>
2019-09-04 17:38:35 +0800

    Support for promise / await

--- index.js
+++ index.js
@@ -5,30 +5,56 @@ module.exports = function basicPlugin(pluginConf, web, next) {
   var SysPar = web.includeModel(pluginConf.sysParModel)
   web.syspars = new Object();
   web.syspars.get = function(key, cb) {
-    SysPar.findOne({key: key}, function(err, syspar) {
-      cb(err, syspar);
-    })
-  }
-
-  web.syspars.set = function(key, value, cb) {
-    SysPar.findOne({key: key}, function(err, syspar) {
-      if (err) {
-        throw err;
-      }
-
-      if (!syspar) {
-        syspar = new SysPar();
-      }
-
-      syspar.key = key;
-      syspar.val = value;
-      syspar.save(function(err) {
+    return new Promise(function(resolve, reject) {
+      SysPar.findOne({key: key}, function(err, syspar) {
         if (cb) {
           cb(err, syspar);
         }
+        
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(syspar);
+      })  
+    });
+    
+  }
+
+  web.syspars.set = function(key, value, cb) {
+    return new Promise(function(resolve, reject) {
+      SysPar.findOne({key: key}, function(err, syspar) {
+        if (err) {
+          if (cb) {
+            cb(err);
+          }
+          reject(err);
+          throw err;
+        }
+
+        if (!syspar) {
+          syspar = new SysPar();
+        }
+
+        syspar.key = key;
+        syspar.val = value;
+        syspar.save(function(err) {
+          if (cb) {
+            cb(err, syspar);
+          }
+
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve(syspar);
+        })
+        
       })
-      
-    })
+    });
+    
   }
   
   next();
